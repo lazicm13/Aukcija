c6d7600ae6a19e83e3642c1d5ad8111c333c5d8f
Marko <lazicm714@gmail.com>
2025-03-03 18:29:44 +0100

    csrf problem fixing

--- VelikaAukcija/backend/settings.py
+++ VelikaAukcija/backend/settings.py
@@ -115,7 +115,7 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['velikaaukcija.com', "www.velikaaukcija.com", "api.velikaaukcija.com", "velika-aukcija.onrender.com"]
 
